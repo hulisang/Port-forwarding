44b0c612dc16cf9db947760bf2943f56f7e39c62
hulisang <40879374+hulisang@users.noreply.github.com>
2020-12-15 16:56:10 +0800

    Create install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,23 @@
+function installDocker {
+  which docker
+
+  if [ $? -eq 0 ]
+  then
+      docker --version | grep "Docker version"
+      if [ $? -eq 0 ]
+      then
+          echo "docker existing"
+      else
+          curl -fsSL https://get.docker.com | bash
+      fi
+  else
+      curl -fsSL https://get.docker.com | bash
+  fi
+}
+
+installDocker
+
+curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod a+x /usr/local/bin/docker-compose
+rm -rf `which dc`
+ln -s /usr/local/bin/docker-compose /usr/bin/dc
