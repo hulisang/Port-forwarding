fb5b04ddb3364344dfa68e422386bda16d49483f
hulisang <40879374+hulisang@users.noreply.github.com>
2022-06-18 14:23:55 +0800

    Update install-docker.sh
    update docker-compose version to 2.6.0

--- install-docker.sh
+++ install-docker.sh
@@ -17,7 +17,7 @@ function installDocker {
 
 installDocker
 
-curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/2.6.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod a+x /usr/local/bin/docker-compose
 rm -rf `which dc`
 ln -s /usr/local/bin/docker-compose /usr/bin/dc
